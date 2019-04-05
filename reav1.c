4577d9144bbd4505a42339adb92d13c4a90047fd
RCorrea <47988061+ruancorrea@users.noreply.github.com>
2019-04-05 12:14:20 -0300

    Create reav1.c

---
+++ reav1.c
@@ -0,0 +1,129 @@
+#include <stdio.h>
+#include <string.h>
+#include <math.h>
+#include <stdlib.h>
+typedef struct btree btree;
+typedef struct queue queue;
+typedef unsigned char byte;
+typedef struct node node;
+
+struct btree
+{
+    byte c;
+    btree *left;
+    btree *right;
+};
+
+struct queue
+{
+    node *head;
+};
+
+struct node
+{
+    byte item;
+    long int priority;
+    node *next;
+};
+
+btree* create_empty_btree(){
+    return NULL;
+}
+
+
+queue* create_queue()
+{
+    queue *new_queue = (queue*) malloc(sizeof(queue));
+    new_queue->head = NULL;
+    return new_queue;
+}
+
+void print(queue *fila)
+{
+    node *aux = fila->head;
+    if(aux != NULL)
+    {
+        printf("%d %c\n",aux->priority, aux->item);
+        aux = aux->next;
+    }
+}
+
+void enqueue(queue *pq, byte item, long int freq)
+{
+    node *new_node = (node*) malloc(sizeof(node));
+    new_node->item = item;//o//v
+    new_node->priority = freq;//2//1
+    
+    if (pq->head == NULL || freq < pq->head->priority) {
+        printf("oi\n");
+        new_node->next = pq->head;
+        pq->head = new_node;
+    } else {
+        node *current = pq->head;
+        while (current->next != NULL && current->next->priority < freq) {
+            current = current->next;
+        }
+        new_node->next = current->next;
+        current->next = new_node;
+    }
+}
+
+void huff(btree *bt, long int *freq)
+{
+    int i,flag = 0;
+    queue *fila = create_queue();
+    if(fila==NULL) printf("arroz com feijao\n\n");
+    
+    
+    for(i=0;i<256;i++)
+    {
+        if(freq[i] != 0)
+        {
+            /*if(!flag)
+            {
+                fila = (queue*) malloc(sizeof(queue));
+                //fila->head->priority = freq[i];
+                //fila->head->item = i;
+                //fila->head->next = NULL;
+                flag = 1;
+            } else */
+           // printf("%c %d\n",i,freq[i]);
+            enqueue(fila,i,freq[i]);
+            //print(fila);
+        }
+        
+    }
+    print(fila);
+}
+
+int main() {
+    btree *arv = create_empty_btree();
+    char string[256];
+    long int freq[256] = {0};
+    scanf("%s",string);
+    
+    int i,j,tam = strlen(string);
+    
+    for(i=0;i<tam;i++)
+    {  
+        for(j=0;j<256;j++)
+        {
+      
+            if(string[i] == j)
+            {
+                freq[j]++;
+            }
+        }
+    }
+    
+   /* for(i=0;i<256;i++)
+    {
+        if(freq[i] != 0){
+            printf("%c %d\n",i, freq[i]);
+        }
+    }*/
+    
+    huff(arv,freq);
+    
+	return 0;
+}

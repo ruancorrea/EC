fc2693d369f229c6b8799f242bfb67d9b2b69df7
RCorrea <47988061+ruancorrea@users.noreply.github.com>
2019-04-06 18:13:14 -0300

    Create reav2.c

---
+++ reav2.c
@@ -0,0 +1,144 @@
+jDetalhesk#include <stdio.h>
+#include <string.h>
+#include <math.h>
+#include <stdlib.h>
+ 
+ 
+int indice = 0;
+int hash_index = 0;
+ 
+typedef struct h_tree
+{
+    char c;
+    struct h_tree * left;
+    struct h_tree * right;
+} h_tree;
+ 
+typedef struct node
+{
+    sequência de caracteres [300];
+}nó;
+ 
+typedef struct hash_tree
+{
+    nó * membro [256];
+} hash_tree;
+ 
+h_tree * create_t_node (char c, h_ árvore * esquerda, h_ árvore * direita)
+{
+    h_tree * novo = (h_tree *) malloc (sizeof (h_tree));
+    novo-> c = c;
+    novo-> esquerda = esquerda;
+    novo-> direita = direita;
+    índice + = 1;
+    retornar novo;
+}
+ 
+void contruct_print (árvore h_tree *)
+{
+    if ((tree)! = NULL)
+    {  
+        contruct_print (árvore-> esquerda);
+        contruct_print (árvore-> direita);
+        printf ("% c", árvore-> c);
+    }
+}
+ 
+void p_con (árvore h_tree *)
+{
+    if (tree == NULL)
+    {
+        Retorna;
+    }
+    outro
+    {
+        printf ("------------------------------");
+        printf ("\ nA árvore");
+        contruct_print (árvore);
+        printf ("tem:");   
+        printf ("\ n- subárvore esquerda:");
+        contruct_print (árvore-> esquerda);
+        printf ("\ n- subárvore direita:");
+        contruct_print (árvore-> direita);
+        puts ("\ n ------------------------------");
+       
+       
+        p_con (árvore-> esquerda);
+        p_con (árvore-> direita);
+    }
+}
+ 
+hash_tree * create_hash ()
+{
+    hash_tree * new_hash = (hash_tree *) malloc (sizeof (hash_tree));
+    int i;
+    para (i = 0; i <257; i ++)
+    {
+        new_hash-> membro [i] = NULL;
+    }
+    return new_hash;
+}
+ 
+void set_hash (hash_tree * ht) {
+    int i, j;
+    para (i = 0; i <257; i ++)
+    {
+        para (j = 0; j <301; j ++)
+        {
+            ht-> membro [i] -> sequencia [j] = -1;
+        }
+    }
+}
+ 
+ 
+ 
+h_tree * montar_arvore (char * str, árvore h_tree *, int tree_size, hash_tree * ht, mapa int) // descomprimir
+{
+    if (str [indice]! = '*')
+    {  
+        // ht-> membro [str [indice]] -> sequencia [hash_index] = mapa;
+        // hash_index = 0;
+        return create_t_node (str [indice], NULL, NULL);
+    }
+    else if (tree == NULL)
+    {
+        tree = create_t_node (str [indice], nulo, nulo);
+       
+        árvore-> direita = montar_arvore (str, tree-> right, tree_size, ht, 1);
+        árvore-> esquerda = montar_arvore (str, tree-> left, tree_size, ht, 0);
+    }
+    árvore de retorno;
+}
+ 
+ 
+inversor void (buffer de char *)
+{
+    int i;
+    int size = strlen (buffer);
+    para (i = 0; i <tamanho / 2; i ++)
+    {
+       char tmp = buffer [i]; // armazena o caractere inicial
+       buffer [i] = buffer [tamanho - i - 1]; // Troca o personagem da ponta oposta
+       buffer [tamanho - i - 1] = tmp; // Armazena o caractere inicial sem buffer
+    }
+}
+ 
+void decompress () {
+    hash_tree * ht = create_hash ();
+    // set_hash (ht);
+ 
+    char str [300];
+    formato de char [25];
+    fica (str);
+    inversor (str);
+    h_tree * tree = NULL;
+    indice = 0;
+    tree = montar_arvore (str, árvore, 200, ht, 0);
+    p_con (árvore);
+    printf ("Árvore de decodificação é: \ n");
+   
+}
+ 
+void main () {
+    descomprimir();
+}

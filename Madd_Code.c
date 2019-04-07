acf14b77198597c3205cf8dcb2a6f641707bbd7b
RCorrea <47988061+ruancorrea@users.noreply.github.com>
2019-04-06 23:36:03 -0300

    Update Madd_Code.c

--- Madd_Code.c
+++ Madd_Code.c
@@ -2,8 +2,8 @@
 #include <string.h>
 #include <math.h>
 #include <stdlib.h>
-
 typedef struct btree btree;
+
 struct btree{
     char item;
     btree *left;
@@ -15,7 +15,7 @@ btree* create_empty_binary_tree()
     return NULL;
 }
 
-btree* create_binary_tree(char item, btree *left, btree *right)
+btree* create_btree(char item, btree *left, btree *right)
 {
     btree *new_binary_tree = (btree*) malloc(sizeof(btree));
     new_binary_tree->item = item;
@@ -24,36 +24,30 @@ btree* create_binary_tree(char item, btree *left, btree *right)
     return new_binary_tree;
 }
 
-btree* pre_order(btree *bt, char *string, int *i)
+btree* pre_order(btree *bt, char *s)
 {
 
-    if(*i < 27)
+    if(bt == NULL)
     {
-        bt = create_binary_tree(string[*i], NULL, NULL);
-         *i += 1;
-    }
+        bt = create_btree(s,NULL,NULL);
+    }else if(s < bt->item)
+        {
+            bt->left = pre_order(bt->left,s);
+        }else if(s > bt->item)
+        {
+            bt->right = pre_order(bt->right,s);
+        }
     
     return bt;
-}
-
-void print(btree *bt)
-{
-    if(bt != NULL)
-    {
-        printf("%c",bt->item);
-        print(bt->left);
-        //print(bt->right);
-    }
 }
 
 void conversao(btree *arv, char *code)
 {
     int i=0,size;
     size = strlen(code);
-    printf("%d  ",size);
     btree *aux = arv;
-    //for(i=0;i<size;i++)
-    //{
+    for(i=0;i<size;i++)
+    {
         while(1)
         {
             if(code[i] == '*') break;
@@ -61,32 +55,29 @@ void conversao(btree *arv, char *code)
             {
                 aux = aux->left;
             }
-            if(code[i] == '||')
+            if(code[i] == '|')
             {
                 aux = aux->right;
             }
             i++;
         }
-        //i--;
         printf("%c",aux->item);
         aux = arv;
-    //}
+    }
 }
 
 int main() {
     btree *arv = create_empty_binary_tree();
     char geradora[28] = "MGTCKQWAEILORUY BDFHJNPSVXZ";
-    
     char code[2000];
     scanf("%s",code);
     
-    int i=0;
+    int i;
     
-    arv = pre_order(arv,geradora,&i);
-    arv= arv->right;
-    printf("%c ",arv->item);
+    for(i=0;i<strlen(geradora);i++) arv = pre_order(arv,geradora[i]);
     
-   // print(arv);
-   // conversao(arv,code);
+    i=0;
+
+    conversao(arv,code);
 	return 0;
 }

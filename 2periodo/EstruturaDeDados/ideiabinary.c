bf1c88cd5e34c90c3e4506322810cd53cb99a61d
RCorrea <47988061+ruancorrea@users.noreply.github.com>
2019-05-30 11:59:09 -0300

    Create ideiabinary.c

---
+++ 2periodo/EstruturaDeDados/ideiabinary.c
@@ -0,0 +1,61 @@
+include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <math.h>
+#include <stdbool.h>
+
+/*char palindromo(char *cod, int tam)
+{
+    char aux[tam],j=0;
+    for(i=tam-1;i>=0;i--)
+    {
+        aux[j] = cod[i];
+        j++;
+    }
+    return aux;
+}*/
+
+char add_l(char *cod, int *i)
+{
+    cod[*i] = '0';
+    *i+=1;
+
+    return cod;
+}
+
+char add_r(char *cod, int *i)
+{
+    cod[*i] = '1';
+    *i+=1;
+
+    return cod;
+}
+
+void codigo(bt *arv, char *cod, int *i)
+{
+    if(arv->left == NULL && arv->right == NULL)
+    {
+        hash = (arv->c, cod);
+        *i -= 1;
+    }
+
+    codigo(arv->left, add_l(cod,i), i);
+    codigo(arv->right, add_r(cod,i), i);
+
+    *i -=1 ;
+}
+
+int main()
+{
+    int i=0;
+
+    bt *huf = blabla(huf);//ondestamos
+
+    char cod[100]; // sla quanto
+
+    cod[0] = 0;
+
+    codigo(huf, cod, &i);
+}
+
+//https://hastebin.com/hasodaxutu.cpp
